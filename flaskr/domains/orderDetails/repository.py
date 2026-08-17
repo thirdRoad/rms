1ad2135839e9bc6d43a43112ee26597474e70a1f
Kaan Üstüntaş <kaan-ustuntas3@hotmail.com>
2026-08-17 13:10:14 +0300

    line alignment correction

--- flaskr/domains/orderDetails/repository.py
+++ flaskr/domains/orderDetails/repository.py
@@ -49,20 +49,20 @@ class OrderDetailsRepository(BaseRepository[OrderDetail]):
         return True
 
     def upsert_item(
-            self, order_id: int, product_id: int, quantity: int, unit_price: float
-        ):
-            item = db.session.get(self.model, (order_id, product_id))
+        self, order_id: int, product_id: int, quantity: int, unit_price: float
+    ):
+        item = db.session.get(self.model, (order_id, product_id))
 
-            if item:
-                item.quantity = quantity
-            else:
-                item = OrderDetail(
-                    order_id=order_id,
-                    product_id=product_id,
-                    quantity=quantity,
-                    unit_price=unit_price,
-                )
-                db.session.add(item)
+        if item:
+            item.quantity = quantity
+        else:
+            item = OrderDetail(
+                order_id=order_id,
+                product_id=product_id,
+                quantity=quantity,
+                unit_price=unit_price,
+            )
+            db.session.add(item)
 
-            db.session.flush()
-            return item
+        db.session.flush()
+        return item
